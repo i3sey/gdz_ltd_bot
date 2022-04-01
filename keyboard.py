b298fe0fb7788b8089d9152f0ca890c36286ab06
issey <deniskedrovsky@mail.ru>
2022-04-01 22:11:23 +0500

    Прибрался, добавил TODO

--- keyboard.py
+++ keyboard.py
@@ -2,21 +2,24 @@ from aiogram import Bot, types
 from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
 ######################################################
-start = types.ReplyKeyboardMarkup(resize_keyboard=True) # СОЗДАЕМ ВООБЩЕ ОСНОВУ ДЛЯ КНОПОК
+start = types.ReplyKeyboardMarkup(resize_keyboard=True) # создаем основы для кнопок
+arrows = InlineKeyboardMarkup() 
+stats = InlineKeyboardMarkup()    
 
-info = types.KeyboardButton("Информация")            # ДОБАВЛЯЕМ КНОПКУ ИНФОРМАЦИИ
-#stats = types.KeyboardButton("Статистика")            # ДОБАВЛЯЕМ КНОПКУ СТАТИСТИКИ
-let = types.KeyboardButton("Получить гдз")
-creator = types.KeyboardButton("Создатель")
+info = types.KeyboardButton("Информация")            #    Добавляем кнопку информации
+let = types.KeyboardButton("Получить гдз")           #    Основная кнопка
+creator = types.KeyboardButton("Создатель")          #    тут я
 
-start.add(let)
-start.add(info, creator) #ДОБАВЛЯЕМ ИХ В БОТА
+start.add(let)                    #добавляем их в прорядке правильном
+start.add(info, creator) 
 
-stats = InlineKeyboardMarkup()    # СОЗДАЁМ ОСНОВУ ДЛЯ ИНЛАЙН КНОПКИ
-stats.add(InlineKeyboardButton(f'Да', callback_data = 'Да')) # СОЗДАЁМ КНОПКУ И КАЛБЭК К НЕЙ
-stats.add(InlineKeyboardButton(f'Нет', callback_data = 'Нет')) # СОЗДАЁМ КНОПКУ И КАЛБЭК К НЕЙ
+stats.add(InlineKeyboardButton(f'Да', callback_data = 'Да')) # А тут кнопочки инлайновые, с каллбеком
+stats.add(InlineKeyboardButton(f'Нет', callback_data = 'Нет')) 
 
-arrows = InlineKeyboardMarkup() #сделал листалку
+
+#TODO:
+#заменить неинформативные эмодзи на цифры уроков:
+
 arrows.add(
     InlineKeyboardButton(f'⬅️', callback_data = 'Предыдущиий номер'),
     InlineKeyboardButton(f'❌', callback_data = 'Удалить'),
