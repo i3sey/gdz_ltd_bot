4f311407dfca9102bd54873fad9d61c2fbf88fb1
issey <deniskedrovsky@mail.ru>
2022-04-02 15:06:37 +0500

    пофиксил pylint'ом

--- keyboard.py
+++ keyboard.py
@@ -1,31 +1,31 @@
-from aiogram import Bot, types
-from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram import types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 ######################################################
 start = types.ReplyKeyboardMarkup(resize_keyboard=True) # создаем основы для кнопок
-arrows = InlineKeyboardMarkup() 
-stats = InlineKeyboardMarkup()    
+arrows = InlineKeyboardMarkup()
+stats = InlineKeyboardMarkup()
 
 info = types.KeyboardButton("Информация")            #    Добавляем кнопку информации
 let = types.KeyboardButton("Получить гдз")           #    Основная кнопка
 creator = types.KeyboardButton("Создатель")          #    тут я
 
 start.add(let)                    #добавляем их в прорядке правильном
-start.add(info, creator) 
+start.add(info, creator)
 
-stats.add(InlineKeyboardButton(f'Да', callback_data = 'Да')) # А тут кнопочки инлайновые, с каллбеком
-stats.add(InlineKeyboardButton(f'Нет', callback_data = 'Нет')) 
+stats.add(InlineKeyboardButton('Да', callback_data = 'Да')) # А тут кнопочки инлайновые, с каллбеком
+stats.add(InlineKeyboardButton('Нет', callback_data = 'Нет'))
 
 
-#TODO:
+#TO-DO:
 #заменить неинформативные эмодзи на цифры уроков:
 
 arrows.add(
-    InlineKeyboardButton(f'⬅️', callback_data = 'Предыдущиий номер'),
-    InlineKeyboardButton(f'❌', callback_data = 'Удалить'),
-    InlineKeyboardButton(f'➡️', callback_data = 'Следующий номер')
+    InlineKeyboardButton('⬅️', callback_data = 'Предыдущиий номер'),
+    InlineKeyboardButton('❌', callback_data = 'Удалить'),
+    InlineKeyboardButton('➡️', callback_data = 'Следующий номер')
     )
 arrows.add(
-    InlineKeyboardButton(f'⬅️', callback_data = 'Предыдущии урок'),
-    InlineKeyboardButton(f'➡️', callback_data = 'Следующий урок')
+    InlineKeyboardButton('⬅️', callback_data = 'Предыдущии урок'),
+    InlineKeyboardButton('➡️', callback_data = 'Следующий урок')
 )
